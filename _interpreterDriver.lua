4d3e05f36d2a5df6943fd14e497cd4dba3a56498
Julian Contreras <jrcontre@mit.edu>
2017-08-08 19:56:53 -0400

    first base

--- _interpreterDriver.lua
+++ _interpreterDriver.lua
@@ -2,7 +2,7 @@ require("base.lexer")
 require("base.parser")
 require("base.interpreter")
 
-filename = "samples/b1.flang"
+filename = "samples/complete.flang"
 local f = assert(io.open(filename, "r"))
 local t = f:read("*all")
 f:close()
