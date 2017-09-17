006bc5e2006a049e936f81903c9e0de9a43543a1
Julian Contreras <jrcontre@mit.edu>
2017-09-17 19:38:26 -0400

    lit

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -6,24 +6,14 @@ flangChip.icon = "__Flang__/graphics/flangchip.png"
 flangChip.flags = { "goes-to-quickbar" }
 flangChip.subgroup = "circuit-network"
 flangChip.place_result="flang-chip"
-flangChip.order = "c[combinators]-a[arithmetic-combinator]"
 flangChip.stack_size= 50
 
-local recipe = table.deepcopy(data.raw.recipe["arithmetic-combinator"])
+local recipe = {}
+recipe.type = "recipe"
+recipe.name = "flang-chip"
 recipe.enabled = true
 recipe.ingredients = {{"copper-plate",200},{"steel-plate",50}}
 recipe.result = "flang-chip"
-
--- local recipe = {}
--- recipe.type = "recipe"
--- recipe.name = "flang-chip"
--- recipe.enabled = true
--- recipe.ingredients =
--- {
---   {"copper-cable", 5},
---   {"electronic-circuit", 5},
--- }
--- recipe.result = "flang-chip"
 
 data:extend{flangChip, recipe}
 
